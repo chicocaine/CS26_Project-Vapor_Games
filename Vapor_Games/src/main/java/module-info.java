90cdb4b57fe2fb761599e97e0bb2aa201f7b6a0a
HanazonoArchive <jaymark23x@gmail.com>
2024-11-13 20:14:13 +0800

    JavaFX Addition

---
+++ Vapor_Games/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module visual.vapor_games {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens visual.vapor_games to javafx.fxml;
+    exports visual.vapor_games;
+}
