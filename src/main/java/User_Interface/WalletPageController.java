6c6df6ce362a7b277c05822e7b763cea36da8801
HanazonoArchive <jaymark23x@gmail.com>
2024-12-02 23:02:59 +0800

    DROPDOWN MENU ADDED and WALLET AND ACCOUNT PAGE GOODS!

---
+++ src/main/java/User_Interface/WalletPageController.java
@@ -0,0 +1,25 @@
+package User_Interface;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class WalletPageController {
+
+    @FXML
+    private Label WalleCurrentAGSCoin_Label;
+
+    @FXML
+    private Button WalletInputCodeButton_Button;
+
+    @FXML
+    private TextField WalletInputCode_TextField;
+
+    @FXML
+    void HandlesButtonClicked(MouseEvent event) {
+
+    }
+
+}
